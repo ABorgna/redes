01f7cdb3982bdea93eedb3da11a99d4bce8ecc07
gflan <twety.vg@gmail.com>
2017-05-09 23:10:33 -0300

    Cuentas

--- tp1/src/distinguidos.py
+++ tp1/src/distinguidos.py
@@ -10,39 +10,52 @@ from math import log2
 WHO_HAS = 1
 
 
-def nuevo_paquete(fuente, total):
+def nuevo_paquete(fuente_apariciones, total):
 # el handler de prn solo puede tener un parametro libre (y por lo tanto no
 # deja pasar el diccionario y el total) por hay que anidar:
     def contar_paquete(paquete):
-        nonlocal fuente, total
+        nonlocal fuente_apariciones, total
         if paquete[sc.ARP].op == WHO_HAS:
-            buscado = paquete[sc.ARP].pdst
-            fuente[buscado] = 1 if buscado not in fuente else fuente[buscado] + 1
+            buscado = paquete[sc.ARP].pdst # es la ip del request
+            fuente_apariciones[buscado] = 1 if buscado not in fuente_apariciones else fuente_apariciones[buscado] + 1
             total += 1
     print()
     return contar_paquete
 
 if __name__ == '__main__':
 
-    fuente = {}
+    fuente_apariciones = {}
     total = 0
 
     # cargamos pcap o escuchamos por interfaces:
-
     try:
         if len(sys.argv) > 1:
+            path = sys.argv[1]
             try:
                 pkts = sc.rdpcap(path)
             except FileNotFoundError as e:
                 print(e, file=sys.stderr)
                 sys.exit()
-            for p in pkts: nuevo_paquete(fuente, total)(p)
+            for p in pkts: nuevo_paquete(fuente_apariciones, total)(p)
         else:
             print("Interrumpir con ctrl-C")
-            sc.sniff(prn=nuevo_paquete(fuente, total), count=0, store=0, filter="arp")
+            sc.sniff(prn=nuevo_paquete(fuente_apariciones, total), count=0, store=0, filter="arp")
 
     except KeyboardInterrupt:
             print("Terminó de sniffear")
 
-    # TODO ahora hacemos las cuentitas:
-    # TODO definir noción de distinguido
+    #  ahora hacemos las cuentitas
+    if total == 0:
+        print("No se encontraron paquetes ARP del tipo who-has")
+        sys.exit()
+
+
+    # siempre la proba es mayor a 0 (porque estan en el diccionario)
+    fuente_probabilidades = {s: fuente_apariciones[s] / total for s in fuente_apariciones}
+    fuente_informacion = {s: -log2(fuente_probabilidades[s]) for s in fuente_probabilidades}
+
+    fuente_entropia = sum(map(lambda s: fuente_probabilidades[s]*fuente_informacion[s], fuente_apariciones))
+
+    for simbolo in fuente_informacion:
+        if fuente_entropia > fuente_informacion[simbolo]:
+            print("Nodo distinguido: {d} ".format(simbolo))
