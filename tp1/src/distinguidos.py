60eecea2100db5a19ad138fbb84e123fb2ca9ca4
gflan <twety.vg@gmail.com>
2017-05-10 01:31:13 -0300

    Funciona el ej2

--- tp1/src/distinguidos.py
+++ tp1/src/distinguidos.py
@@ -12,20 +12,21 @@ WHO_HAS = 1
 
 def nuevo_paquete(fuente_apariciones, total):
 # el handler de prn solo puede tener un parametro libre (y por lo tanto no
-# deja pasar el diccionario y el total) por hay que anidar:
+# deja pasar el diccionario y el total) por eso hay que anidar:
     def contar_paquete(paquete):
         nonlocal fuente_apariciones, total
-        if paquete[sc.ARP].op == WHO_HAS:
+        if sc.ARP in paquete and paquete[sc.ARP].op == WHO_HAS:
             buscado = paquete[sc.ARP].pdst # es la ip del request
             fuente_apariciones[buscado] = 1 if buscado not in fuente_apariciones else fuente_apariciones[buscado] + 1
-            total += 1
-    print()
+            total[0] += 1
+            print("Who-has con destino {}".format(buscado))
+        return
     return contar_paquete
 
 if __name__ == '__main__':
 
     fuente_apariciones = {}
-    total = 0
+    total = [0] # para que sea mutable
 
     # cargamos pcap o escuchamos por interfaces:
     try:
@@ -44,11 +45,13 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
             print("Terminó de sniffear")
 
+    total = total[0]
     #  ahora hacemos las cuentitas
     if total == 0:
         print("No se encontraron paquetes ARP del tipo who-has")
         sys.exit()
 
+    print()
 
     # siempre la proba es mayor a 0 (porque estan en el diccionario)
     fuente_probabilidades = {s: fuente_apariciones[s] / total for s in fuente_apariciones}
@@ -58,4 +61,4 @@ if __name__ == '__main__':
 
     for simbolo in fuente_informacion:
         if fuente_entropia > fuente_informacion[simbolo]:
-            print("Nodo distinguido: {d} ".format(simbolo))
+            print("Nodo distinguido {}".format(simbolo))
