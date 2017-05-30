bda33705100a655f74229a0f63c8c37e8fbae352
gflan <twety.vg@gmail.com>
2017-05-30 14:40:02 -0300

    Ahora funca

--- tp2/src/traceroute.py
+++ tp2/src/traceroute.py
@@ -2,13 +2,13 @@ import scapy.all as sc
 from time import time
 
 TIMEOUT = 5
-MIN_TTL=10
-MAX_TTL=10
+MIN_TTL=1
+MAX_TTL=30
 
 RESULTS=0
 UNANSWERED=1
 
-times = {}
+times = []
 
 DST = ["www.google.com"]
 
@@ -16,15 +16,19 @@ packet = sc.IP(dst=DST,ttl=(MIN_TTL,MAX_TTL))/sc.ICMP()
 
 
 for p in packet:
-    initial_t = time()*1000 # Milisegundos
+    initial_t = time()
     ans,unans = sc.sr(p,timeout=TIMEOUT)
-    final_t = time()*1000 - initial_t
+    final_t = time() - initial_t
+    s, r = ans[0]
 
-    print(ans.summary( lambda t,r : r.sprintf("%ICMP.type%") ))
+    if r.haslayer(sc.ICMP) and r.payload.type in [11, 0]: # time-exceeded o reply
+        ip = r.src
+        print(ip)
+        times.append((ip, final_t))
 
-    if (ans.summary( lambda t,r : r.sprintf("%ICMP.type%") )) == "echo-reply":
-        ip = ans.summary( lambda t,r : r.sprintf("%IP.dst%"))
-        times[ip] = final_t 
+        if r.payload.type == 0: # ya llegó a destino
+            break
 
-    
 
+for ip, tiempo in times:
+    print(" -------\n IP: {}\n Tiempo medido: {}".format(ip, tiempo))
