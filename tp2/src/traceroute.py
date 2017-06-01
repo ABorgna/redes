8d4bde3e6e13b4f77879a9a10e69533d8de64370
Agustin Borgna <hello@aborgna.com.ar>
2017-06-01 01:19:14 -0300

    usar generators para que se vaya actualizando

--- tp2/src/traceroute.py
+++ tp2/src/traceroute.py
@@ -11,16 +11,14 @@ UNANSWERED=1
 
 
 def armar_rutas(dst, iteraciones):
-    times = {}
-
     for ttl_actual in range(1, MAX_TTL+1):
-        times[ttl_actual] = []
+        ttl_times = []
         replied = False
 
         for i in range(iteraciones):
             packet = sc.IP(dst=dst,ttl=ttl_actual)/sc.ICMP()
             initial_t = time()
-            ans,unans = sc.sr(packet,timeout=TIMEOUT)
+            ans,unans = sc.sr(packet, timeout=TIMEOUT, verbose=False)
             final_t = (time() - initial_t)*1000
 
             if ans:
@@ -30,23 +28,22 @@ def armar_rutas(dst, iteraciones):
                     ip = r.src
                     #print(ip)
 
-                    times[ttl_actual].append((ip, final_t))
+                    ttl_times.append((ip, final_t))
 
                     if r.payload.type == 0: # ya llegó a destino
                         replied = True
+
+        yield ttl_actual, ttl_times
+
         if replied:
             break
-
-    return times
-
-
 
 if __name__ == '__main__':
     dst = sys.argv[1] if len(sys.argv) > 1 else "www.uwa.edu.au"
     iters = int(sys.argv[2]) if len(sys.argv) > 2 else 30
     times = armar_rutas(dst, iters)
 
-    for ttl, tanda in times.items():
+    for ttl, tanda in times:
         if tanda:
             print(" TTL: {} ----------".format(ttl))
             for ip, tiempo in tanda:
